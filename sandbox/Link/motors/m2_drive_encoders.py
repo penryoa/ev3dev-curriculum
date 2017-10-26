09bb819b312188753f6d713c813d1f4ff9fdb091
thompsm5 <thompsm5@rose-hulman.edu>
2017-10-26 14:11:13 -0400

    folders

--- sandbox/Link/motors/m2_drive_encoders.py
+++ sandbox/Link/motors/m2_drive_encoders.py
@@ -48,7 +48,7 @@ def main():
 
     print("Goodbye!")
     ev3.Sound.speak("Goodbye").wait()
-    
+     
 main()
 
 # DONE: 3. Add a beep after the drive motors stop (see code below).  Test your code to hear the beep AFTER movement.
