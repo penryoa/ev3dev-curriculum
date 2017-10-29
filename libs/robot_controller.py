58513e52d5f39ebf1d0c60a71daf3a18929b69bd
bockke <bockke@rose-hulman.edu>
2017-10-29 14:40:45 -0400

    named it

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -54,24 +54,17 @@ class Snatch3r(object):
             self.left_motor.run_to_rel_pos(position_sp = 11 * degrees_to_turn, speed_sp = turn_speed_sp, stop_action = ev3.Motor.STOP_ACTION_BRAKE)
             self.right_motor.run_to_rel_pos(position_sp = 0, speed_sp = -turn_speed_sp, stop_action = ev3.Motor.STOP_ACTION_BRAKE)
 
-            # self.right_motor.run_to_rel_pos(position_sp = degrees_to_turn,
-            #                                 speed_sp = turn_speed_sp,
-            #                                 stop_action = ev3.Motor.STOP_ACTION_BRAKE)
-
         if degrees_to_turn < 0:
 
-            self.right_motor.run_to_rel_pos(position_sp=degrees_to_turn, speed_sp=turn_speed_sp, stop_action = ev3.Motor.STOP_ACTION_BRAKE)
-            self.left_motor.run_to_rel_pos(position_sp=degrees_to_turn, speed_sp=-turn_speed_sp, stop_action = ev3.Motor.STOP_ACTION_BRAKE)
+            self.right_motor.run_to_rel_pos(position_sp=0, speed_sp=turn_speed_sp, stop_action = ev3.Motor.STOP_ACTION_BRAKE)
+            self.left_motor.run_to_rel_pos(position_sp=11* degrees_to_turn, speed_sp=-turn_speed_sp, stop_action = ev3.Motor.STOP_ACTION_BRAKE)
 
-            # self.left_motor.run_to_rel_pos(position_sp = degrees_to_turn,
-            #                                 speed_sp = turn_speed_sp,
-            #                                 stop_action = ev3.Motor.STOP_ACTION_BRAKE)
         self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
         self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
 
 
         ev3.Sound.beep().wait()
-        ev3.Sound.speak("Hi my name is Linda and maybe I will turn.").wait()
+        
 
 
 
