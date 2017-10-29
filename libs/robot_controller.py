a17e24168ffd446deaaffee7a816fa3ea8b7451f
bockke <bockke@rose-hulman.edu>
2017-10-29 14:27:37 -0400

    named it

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -57,8 +57,8 @@ class Snatch3r(object):
             # self.right_motor.run_to_rel_pos(position_sp = degrees_to_turn,
             #                                 speed_sp = turn_speed_sp,
             #                                 stop_action = ev3.Motor.STOP_ACTION_BRAKE)
-            self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
-            self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
+        self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
+        self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
 
         if degrees_to_turn < 0:
 
@@ -68,8 +68,8 @@ class Snatch3r(object):
             # self.left_motor.run_to_rel_pos(position_sp = degrees_to_turn,
             #                                 speed_sp = turn_speed_sp,
             #                                 stop_action = ev3.Motor.STOP_ACTION_BRAKE)
-            self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
-            self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
+        self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
+        self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
 
 
         ev3.Sound.beep().wait()
