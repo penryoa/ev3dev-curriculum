4a313229578f9f1546e2ecb889ca92552d7f2fcd
penryoa <oliviapenry@gmail.com>
2017-10-30 14:35:15 -0400

    Linda the Escape Artist

--- sandbox/Link/digital_inputs/m4_touch_sensor_arm_movement.py
+++ sandbox/Link/digital_inputs/m4_touch_sensor_arm_movement.py
@@ -77,8 +77,8 @@ def arm_calibration(arm_motor, touch_sensor):
     while False:
         if touch_sensor.is_pressed:
             time.sleep(0.01)
-        arm_motor.stop(stop_action = arm_motor.STOP_ACTION_BRAKE)
-
+    arm_motor.stop(stop_action = arm_motor.STOP_ACTION_BRAKE)
+    ev3.Sound.beep()
     arm_revolutions_for_full_range = 14.2
     arm_motor.run_to_rel_pos(position_sp=-arm_revolutions_for_full_range)
     arm_motor.wait_while(ev3.Motor.STATE_STALLED)
