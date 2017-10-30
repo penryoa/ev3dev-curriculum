de879d0376243349191c21a760353aebe0954ef1
penryoa <oliviapenry@gmail.com>
2017-10-30 14:39:00 -0400

    Linda the Escape Artist

--- sandbox/Link/digital_inputs/m4_touch_sensor_arm_movement.py
+++ sandbox/Link/digital_inputs/m4_touch_sensor_arm_movement.py
@@ -31,8 +31,7 @@ def main():
         command_to_run = input("Enter c (for calibrate), u (for up), d (for down), or q (for quit): ")
         if command_to_run == 'c':
             print("Calibrate the arm")
-            print("TODO: 3 is to delete this print statement, uncomment the line below, and implement that function.")
-            # arm_calibration(arm_motor, touch_sensor)
+            arm_calibration(arm_motor, touch_sensor)
         elif command_to_run == 'u':
             print("Move the arm to the up position")
             print("TODO: 4 is to delete this print statement, uncomment the line below, and implement that function.")
