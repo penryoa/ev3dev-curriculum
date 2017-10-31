4e75cb1f10eb8e165e2d9270a06e2430ed85515d
bockke <bockke@rose-hulman.edu>
2017-10-31 16:43:33 -0400

    named it

--- sandbox/Link/digital_inputs/m4_touch_sensor_arm_movement.py
+++ sandbox/Link/digital_inputs/m4_touch_sensor_arm_movement.py
@@ -122,12 +122,6 @@ def arm_down(arm_motor):
     # Make a beep sound
 
 
-
-    # arm_revolutions_for_full_range = 14.2 * 360
-    # arm_motor.run_to_abs_pos(position_sp=-arm_revolutions_for_full_range,stop_action = ev3.Motor.STOP_ACTION_BRAKE)
-    # arm_motor.wait_while(ev3.Motor.STATE_RUNNING)
-    # ev3.Sound.beep()
-
     arm_revolutions_for_full_range = 14.2 * 360
     arm_motor.run_to_rel_pos(position_sp=-arm_revolutions_for_full_range)
     arm_motor.wait_while(ev3.Motor.STATE_RUNNING)
