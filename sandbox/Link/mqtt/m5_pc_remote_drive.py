7206693e3fefe99a80a6b7fe20330b987c092116
thompsm5 <thompsm5@rose-hulman.edu>
2017-11-02 14:47:41 -0400

    suuuuuuh dude

--- sandbox/Link/mqtt/m5_pc_remote_drive.py
+++ sandbox/Link/mqtt/m5_pc_remote_drive.py
@@ -22,7 +22,7 @@ You will need to have the following features:
 
 You can start by running the code to see the GUI, but don't expect button clicks to do anything useful yet.
 
-Authors: David Fisher and Kaelyn Bock.
+Authors: David Fisher and Maroon5.
 """  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
@@ -55,7 +55,7 @@ def main():
     right_speed_entry.insert(0, "600")
     right_speed_entry.grid(row=1, column=2)
 
-    # TODO: 3. Implement the callbacks for the drive buttons. Set both the click and shortcut key callbacks.
+    # DONE: 3. Implement the callbacks for the drive buttons. Set both the click and shortcut key callbacks.
     #
     # To help get you started the arm up and down buttons have been implemented.
     # You need to implement the five drive buttons.  One has been writen below to help get you started but is commented
@@ -115,9 +115,9 @@ def main():
 # ----------------------------------------------------------------------
 # Tkinter callbacks
 # ----------------------------------------------------------------------
-# TODO: 4. Implement the functions for the drive button callbacks.
+# DONE: 4. Implement the functions for the drive button callbacks.
 
-# TODO: 5. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.  This is the final one!
+# DONE: 5. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.  This is the final one!
 #
 # Observations you should make, you did basically this same program using the IR Remote, but your computer can be a
 # remote control that can do A LOT more than an IR Remote.  We are just doing the basics here.
