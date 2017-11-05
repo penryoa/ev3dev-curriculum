99f78241840dba871ccdaecaeee184d31f9352ae
bockke <bockke@rose-hulman.edu>
2017-11-05 16:51:50 -0500

    named it

--- sandbox/Link/analog_sensors/color_sensor/m2_follow_a_line.py
+++ sandbox/Link/analog_sensors/color_sensor/m2_follow_a_line.py
@@ -27,7 +27,7 @@ def main():
     print("--------------------------------------------")
     ev3.Sound.speak("Follow a line").wait()
 
-    # TODO: 4: After running the code set the default white and black levels to a better initial guess.
+    # DONE: 4: After running the code set the default white and black levels to a better initial guess.
     #   Once you have the values hardcoded to resonable numbers here you don't really need the w and b commands below.
     white_level = 95
     black_level = 2
@@ -37,7 +37,7 @@ def main():
         command_to_run = input("Enter w (white), b (black), f (follow), or q (for quit): ")
         if command_to_run == 'w':
             print("Calibrate the white light level")
-            # TODO: 2. Read the reflected_light_intensity property of the color sensor and set white_level to that value
+            # DONE: 2. Read the reflected_light_intensity property of the color sensor and set white_level to that value
             # As discussed in the prior module, it is recommended that you've added to your Snatch3r class's constructor
             # the color_sensor, as shown:
             #   self.color_sensor = ev3.ColorSensor()
@@ -49,7 +49,7 @@ def main():
             print("New white level is {}.".format(white_level))
         elif command_to_run == 'b':
             print("Calibrate the black light level")
-            # TODO: 3. Read the reflected_light_intensity property of the color sensor and set black_level
+            # DONE: 3. Read the reflected_light_intensity property of the color sensor and set black_level
 
 
 
@@ -80,7 +80,7 @@ def follow_the_line(robot, white_level, black_level):
     left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
     right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
 
-    # TODO: 5. Use the calibrated values for white and black to calculate a light threshold to determine if your robot
+    # DONE: 5. Use the calibrated values for white and black to calculate a light threshold to determine if your robot
     # should drive straight or turn to the right.  You will need to test and refine your code until it works well.
     # Optional extra - For a harder challenge could you drive on the black line and handle left or right turns?
 
