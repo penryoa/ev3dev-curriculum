a756d2eb87841ce8c134752e79306fb3f6d729c4
penryoa <oliviapenry@gmail.com>
2017-11-05 16:39:27 -0500

    Linda the Dragon Slayer Or Mother of Dragons Khaleesi Khalinda

--- sandbox/Link/analog_sensors/ir_sensor/m2_beacon_seeking.py
+++ sandbox/Link/analog_sensors/ir_sensor/m2_beacon_seeking.py
@@ -9,8 +9,8 @@ function gets the robot to that location it will stop the robot and return.  Wit
 prompted if they want to find the beacon again (presumably you move it first) or quit.
 
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and Link.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 import traceback
 
 import ev3dev.ev3 as ev3
@@ -32,7 +32,7 @@ def main():
             result = seek_beacon(robot)
 
 
-            # TODO: 5. Save the result of the seek_beacon function (a bool), then use that value to only say "Found the
+            # DONE: 5. Save the result of the seek_beacon function (a bool), then use that value to only say "Found the
             # beacon" if the return value is True.  (i.e. don't say "Found the beacon" if the attempts was cancelled.)
             if result is True:
                 ev3.Sound.speak("Found the beacon")
@@ -68,7 +68,7 @@ def seek_beacon(robot):
     while not robot.touch_sensor.is_pressed:
         # The touch sensor can be used to abort the attempt (sometimes handy during testing)
 
-        # TODO: 3. Use the beacon_seeker object to get the current heading and distance.
+        # DONE: 3. Use the beacon_seeker object to get the current heading and distance.
         current_heading = beacon_seeker.heading  # use the beacon_seeker heading
         current_distance = beacon_seeker.distance # use the beacon_seeker distance
         if current_distance == -128:
@@ -76,7 +76,7 @@ def seek_beacon(robot):
             print("IR Remote not found. Distance is -128")
             robot.stop()
         else:
-            # TODO: 4. Implement the following strategy to find the beacon.
+            # DONE: 4. Implement the following strategy to find the beacon.
             # If the absolute value of the current_heading is less than 2, you are on the right heading.
             #     If the current_distance is 0 return from this function, you have found the beacon!  return True
             #     If the current_distance is greater than 0 drive straight forward (forward_speed, forward_speed)
