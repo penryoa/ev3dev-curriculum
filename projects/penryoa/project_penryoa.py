3c9f76b77f3b27ec8645493d4cd6e3086a06e357
penryoa <oliviapenry@gmail.com>
2017-11-08 21:09:48 -0500

    Linda the Dragon Slayer Or Mother of Dragons Khaleesi Khalinda

---
+++ projects/penryoa/project_penryoa.py
@@ -0,0 +1,21 @@
+#  ---------  ----      ----  ----     ----  ----      ---
+#  ---------  ----      ----   ----   ----   ----     -----
+#  ---   ---  ----      ----    ---- ----    ----    --   --
+#  ---   ---  ----      ----     -------     ----   ---------
+#  ---------  --------  ----      -----      ----  ---     ---
+#  ---------  --------  ----       ---       ---- ---       ---
+
+"""
+The goal of this project is to get the robot to [blah blah]
+
+"""
+
+import ev3dev.ev3 as ev3
+import time
+import math
+import robot_controller as robo
+import tkinter as tk
+from tkinter import ttk
+import mqtt_remote_method_calls as com
+
+
